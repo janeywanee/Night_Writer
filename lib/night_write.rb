5d92729f522d9fc8ab3ad148b0584076d17902f8
Jane Kim <Janeekim12@gmail.com>
2018-03-25 17:44:07 -0600

    working on making night read and write

--- lib/night_write.rb
+++ lib/night_write.rb
@@ -1,1 +1,13 @@
-
+# handle = File.open(ARGV[0], "r")
+#
+# incoming_message = handle.read
+#
+# handle.close
+#
+# braille_text = incoming_message.upcase
+#
+# writer = File.open(ARGV[1], "w")
+#
+# writer.write(braille_text)
+#
+# writer.close
