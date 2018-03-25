08f30b634786e2afee5220f39b5d0a94c06bd830
Emmie Hayes <emmiehayes2@gmail.com>
2018-03-25 16:49:35 -0600

    File clean up

--- lib/night_write.rb
+++ lib/night_write.rb
@@ -1,16 +1,1 @@
-class Translator
-  attr_reader :letter
 
-  def initialize(letter)
-    @letter = {["a" => "1"], ["b" => "2"]}
-
-  end
-
-  def encrypt(letter)
-    @letter.map do |letter|
-      
-
-
-  end
-
-end
