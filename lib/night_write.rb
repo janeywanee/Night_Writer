723464e40d0a9afde3500a2e017e921f146041da
Jane Kim <Janeekim12@gmail.com>
2018-03-26 16:46:05 -0600

    ignore

--- lib/night_write.rb
+++ lib/night_write.rb
@@ -1,3 +1,38 @@
+require_relative './translator'
+
+class NightWriter
+end
+
+lines_of_text = File.open(ARGV[0], r)
+to_translate = File.read.(lines)
+
+
+
+# require_relative "./enigma.rb"
+# require "date"
+#
+#
+# text = File.open(ARGV[0], "r")
+#
+# enigma = Enigma.new
+#
+# encryption_file = File.open(ARGV[1], "w")
+# encryption_file.write(enigma.encrypt(message))
+# encryption_file.close
+#
+# puts "Created 'encrypted.txt' with the key #{enigma.offset.key} and #{Date.today}"
+
+
+
+
+
+
+
+
+
+
+
+
 # handle = File.open(ARGV[0], "r")
 #
 # incoming_message = handle.read
--- test/night_write_test.rb
+++ test/night_write_test.rb
@@ -3,9 +3,17 @@ require 'minitest/pride'
 require './lib/night_write'
 require 'pry'
 
-class TranslatorTest < Minitest::Test
+class NightWriter < Minitest::Test
 
-  def test_it_exist
-    translator = Translator.new
+  def test_it_exists
+    night_write = NightWriter.new
+
+    assert night_write.filereader
+  end
+
+  def test_it_can_create_an_instance_translator
+    night_write = NightWriter.new
+
+    assert_instance_of Translator, Translator.new
   end
 end
