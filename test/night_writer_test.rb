adb617b669d965b554ffcb27f492ed9e9c0be872
Jane Kim <Janeekim12@gmail.com>
2018-03-26 20:10:41 -0600

    .join works, and new line works

--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -29,23 +29,20 @@ class NightWriterTest < MiniTest::Test
   end
 
   def test_it_can_translate_single_word_to_braille
-    skip
-    skip
     night_write = NightWriter.new("dog")
     assert_equal [["00",".0",".."], ["0.",".0","0."], ["00","00",".."]],
     night_write.to_braille
   end
 
   def test_it_can_tranlate_a_space
-    skip
     night_write = NightWriter.new("i am")
-      assert_equal [[".0","0.",".."],["..","..",".."],["0.","..",".."],["00","..","0."]], night_write.to_braille
+    result = [[".0","0.",".."],["..","..",".."],["0.","..",".."],["00","..","0."]]
+      assert_equal result, night_write.to_braille
   end
 
   def test_it_can_produce_braille_format_line_one
-    skip
     night_write = NightWriter.new("dog")
-    assert_equal ["00", "0.", "00"], night_write.line_one
+    assert_equal "000.00", night_write.line_one
   end
 
   def test_it_can_produce_braille_format_line_two
@@ -62,6 +59,8 @@ class NightWriterTest < MiniTest::Test
     night_write.line_two
     night_write.line_three
     night_write.update_output
+
+      # remove later
     puts night_write.output
 
   end
