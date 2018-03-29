6b2365a68672a8a1d92e363bff4ce49e20f466fc
Emmie Hayes <emmiehayes2@gmail.com>
2018-03-28 19:11:15 -0600

    Translating back to original message

--- lib/translate_to_english.rb
+++ lib/translate_to_english.rb
@@ -2,10 +2,27 @@ require_relative 'dictionary'
 require 'pry'
 
 class TranslateToEnglish
-  attr_reader :dictionary
+  attr_reader :braille
 
   def initialize(braille)
-    @braille = []
-    @dictionary = dictionary
+    @braille = braille
+    @dictionary = Dictionary.new
   end
+
+  def remove_new_line
+    line = @braille.split("\n")
+  end
+
+  def get_key
+    @braille.map do |braille|
+      @dictionary.library
+    end
+  end
+
+
+  #
+  # def to_english
+  #   @braille.map do |braille|
+  #     @dictionary.library[braille]
+
 end
