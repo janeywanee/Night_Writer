aa180fc9ebdee3c20355263de986d2fa88f2d630
Jane Kim <Janeekim12@gmail.com>
2018-03-29 07:44:15 -0600

    moved txt files into root directory, FINAL

--- lib/night_write.rb
+++ lib/night_write.rb
@@ -2,14 +2,14 @@ require './lib/translate_to_braille'
 require 'pry'
 
 
-lines_of_text = File.open('./lib/message.txt', 'r')
+lines_of_text = File.open('message.txt', 'r')
 input = lines_of_text.read.delete("\n")
 num_of_characters = input.length
 lines_of_text.close
 
 translate_to_braille = TranslateToBraille.new(input)
 
-lines_of_text = File.new('./lib/braille.txt', 'w')
+lines_of_text = File.new('braille.txt', 'w')
 lines_of_text.puts(translate_to_braille.update_output)
 
 lines_of_text.close
